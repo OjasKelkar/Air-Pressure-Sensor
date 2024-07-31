17508ecb1bf81db859a6b8e5796c2fba272b7b34
OjasKelkar <145983873+OjasKelkar@users.noreply.github.com>
2024-07-31 21:16:21 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+import os
+import pymongo
+
+@dataclass
+
+class EnvironmentVariable:
+    mongo_db_url:str = os.getenv("MONGO_DB_URL")
+    
+    
+    
+    
+env_var = EnvironmentVariable()
+
+
+mongo_clent = pymongo.MongoClient(env_var.mongo_db_url)
+print("Database Connected")
